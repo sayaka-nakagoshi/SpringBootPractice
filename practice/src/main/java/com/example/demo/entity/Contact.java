ff6fd587db9986187badf0802812ace50d7e5dd7
sayaka nakagoshi <sayakanakagoshi@sayakanoMacBook-Pro.local>
2025-10-07 00:56:41 +0900

    Javaフレームワーク最終課題03

--- practice/src/main/java/com/example/demo/entity/Contact.java
+++ practice/src/main/java/com/example/demo/entity/Contact.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -44,4 +49,13 @@ public class Contact {
 
 	@Column(name = "body", nullable = false)
 	private String body;
+
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
 }
